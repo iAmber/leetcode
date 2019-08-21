0820cd9a8607fddaf9772e999811c8846817921b
Amber <houlingna@meituan.com>
2019-08-21 20:17:11 +0800

    update

--- 7-reverse-integer.js
+++ 7-reverse-integer.js
@@ -2,7 +2,8 @@
  * @param {number} x
  * @return {number}
  */
-var reverse = function(x) {
+// 72 ms
+const reverse = function(x) {
   if (x === 0) return x
   const absValue = Math.abs(x)
   if (absValue < 10) { // 单位数返回本身
@@ -21,3 +22,42 @@ var reverse = function(x) {
   }
   return str
 }
+// 64ms 
+const reverse1 = function(num) {
+  let negative = false;
+  let result = 0;
+  if (num < 0) {
+    negative = true;
+    num = Math.abs(num);
+  }
+  while (num > 0) {
+    mod = num % 10; // mod = 3 // mod = 2 // mod
+    num = Math.floor(num / 10); // num = 12 // num = 1
+    result = result * 10 + mod; // 0 = 0 * 10 + 3 = 0 + 3 = 3 // 3 = 3 * 10 + 2 = 30 + 2 = 32
+  }
+  if (result > 2147483647) return 0;
+  if (negative) return result * -1;
+  return result;
+};
+
+const reverse3 = function(x) {
+  if (x === 0) return x
+  let negative = false;
+  let num = x;
+  if (num < 0) {
+    negative = true;
+    num = Math.abs(num);
+  }
+  if (num < 10) { // 单位数返回本身
+  	return x
+  }
+  let result = 0;
+  while (num > 0) {
+    mod = num % 10; // mod = 3 // mod = 2 // mod
+    num = Math.floor(num / 10); // num = 12 // num = 1
+    result = result * 10 + mod; // 0 = 0 * 10 + 3 = 0 + 3 = 3 // 3 = 3 * 10 + 2 = 30 + 2 = 32
+  }
+  if (result > 2147483647) return 0;
+  if (negative) return result * -1;
+  return result;
+};

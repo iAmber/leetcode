e7e90d60be2a2263cb3e51e833b83d5a9f6a48f3
Amber <houlingna@meituan.com>
2019-09-24 15:54:33 +0800

    50

--- 50-pow-x-n.js
+++ 50-pow-x-n.js
@@ -6,15 +6,13 @@
 const myPow = function(x, n) {
   if (n === 0) return 1
   if (n === 1) return x
-  const isEven = Math.abs(n) % 2 === 0
-  let res = 0
   const m = Math.floor(Math.abs(n) / 2)
   const temp = myPow(x, m)
-  res = temp * temp
-  if (!isEven) {
+  let res = temp * temp
+  if (n % 2 !== 0) { // 指数为奇数
     res = x * res
   }
-  if (n < 0) {
+  if (n < 0) { // 指数为负数
     res = 1 / res
   }
   return res

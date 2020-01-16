1fd58d3c34fd22d06d131cbcd3b69d7a97a6971b
Amber <houlingna@meituan.com>
2020-01-16 11:31:04 +0800

    update 36

--- 36-valid-sudoku.js
+++ 36-valid-sudoku.js
@@ -18,21 +18,68 @@ Output: true
  * @return {boolean}
  */
 const isValidSudoku = function(board) {
-  let row = new Set()
-  let col = new Set()
-  const squar = new Set()
+  let row = []
+  let col = []
+  const squar = []
   for (let i = 0; i < 9; i++) {
-    row = new Set()
     for (let j = 0; j < 9; j++) {
+      if (!row[i]) row[i] = []
+      if (!col[j]) col[j] = []
+      const m = 3 * Math.floor(i / 3) + Math.floor(j / 3)
+      if (!squar[m]) squar[m] = []
       const item = board[i][j]
-      if (item !== ',') {
-        if (!row.has(item)) {
-          row.add(item)
+      if (item !== '.') {
+        if (row[i].includes(item) || col[j].includes(item) || squar[m].includes(item)) {
+          return false
         }
-        if (col.has(item)) return false
-        if (squar.has(item)) return false
+        row[i].push(item)
+        col[j].push(item)
+        squar[m].push(item)
       }
     }
   }
   return true
 };
+
+// another
+
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+const isValidSudoku = function(board) {
+  let seen = new Set()
+  for (let i = 0; i < 9; ++i) {
+    for (let j = 0; j < 9; ++j) {
+      let number = board[i][j]
+      if (number != '.')
+        if (
+          !hset(seen, number + ' in row ' + i) ||
+          !hset(seen, number + ' in column ' + j) ||
+          !hset(seen, number + ' in block ' + ~~(i / 3) + '-' + ~~(j / 3))
+        )
+        return false
+    }
+  }
+  return true
+}
+function hset(s, val) {
+  if (s.has(val)) return false
+  else {
+    s.add(val)
+    return true
+  }
+}
+
+var a = [
+  ["8","3",".",".","7",".",".",".","."],
+  ["6",".",".","1","9","5",".",".","."],
+  [".","9","8",".",".",".",".","6","."],
+  ["8",".",".",".","6",".",".",".","3"],
+  ["4",".",".","8",".","3",".",".","1"],
+  ["7",".",".",".","2",".",".",".","6"],
+  [".","6",".",".",".",".","2","8","."],
+  [".",".",".","4","1","9",".",".","5"],
+  [".",".",".",".","8",".",".","7","9"]
+]
+console.log('isValidSudoku:', isValidSudoku(a))

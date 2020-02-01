d387c756238149fe9f36b9056e70e8cebc0887a5
Amber <houlingna@meituan.com>
2020-02-01 23:45:57 +0800

    27

--- 27-remove-element.js
+++ 27-remove-element.js
@@ -5,3 +5,26 @@ Your function should return length = 2, with the first two elements of nums bein
 
 It doesn't matter what you leave beyond the returned length.
 */
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+const removeElement = function(nums, val) {
+  let j = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[j] = nums[i];
+      j++;
+    }
+  }
+  return j;
+};
+const removeElement = function(nums, val) {
+  for (let i = 0; i < nums.length; i++) {
+    if (val === nums[i]) {
+      nums.splice(i,1)
+      i--
+    }
+  }
+};

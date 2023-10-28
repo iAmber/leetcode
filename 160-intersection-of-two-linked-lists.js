ac19feed5877992f152be6fabb0507207a0f5063
iAmber <957890374@qq.com>
2023-10-28 17:37:45 +0800

    feat: add 160-intersection-of-two-linked-lists.js

---
+++ 160-intersection-of-two-linked-lists.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=160 lang=javascript
+ *
+ * [160] Intersection of Two Linked Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+    if (!headA || !headB) return null
+    let l1 = headA, l2 = headB
+    while (l1 !== l2) {
+        l1 = l1 ? l1.next : headB
+        l2 = l2 ? l2.next : headA
+    }
+    return l1
+
+};
+// @lc code=end
+
